2168ecbacdf7b5c72c87345e0a59d8f15eedbcb2
Sitong <nickren93@gmail.com>
2025-02-22 14:27:42 -0500

    complete lab

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
@@ -9,6 +9,38 @@ function Question({ question, onAnswered }) {
     setTimeRemaining(10);
     onAnswered(isCorrect);
   }
+  
+  
+  useEffect(() => {
+    /*
+    let timeOutFunction
+    if(timeRemaining>0){
+      timeOutFunction = setTimeout(() => {
+        setTimeRemaining(timeRemaining-1)}, 1000)
+    }else{
+      timeOutFunction = setTimeRemaining(10)
+      onAnswered(false)
+    }
+
+    return function cleanup(){
+      clearTimeout(timeOutFunction)
+    }
+    */
+    const timeOutFunction = setTimeout(() => {
+      if(timeRemaining > 1){
+        setTimeRemaining(timeRemaining-1)
+      }else{
+        setTimeRemaining(10)
+        onAnswered(false)
+      }
+    }, 1000)
+    
+    return function cleanup(){
+      clearTimeout(timeOutFunction)
+    }
+
+  }, [timeRemaining])
+  
 
   const { id, prompt, answers, correctIndex } = question;
 
